33903f644534f727428f681ead76e9fdaab8d6b7
Oguzhan Ozkaya <oguzdt@gmail.com>
2022-07-08 18:51:40 +0300

    swagger

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
 import { NestFactory } from '@nestjs/core';
-import { ValidationPipe } from '@nestjs/common';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerDocumentOptions, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  initDocument(app);
+
   await app.listen(3000);
 }
+
+function initDocument(app: INestApplication) {
+  const config = new DocumentBuilder()
+    .setTitle('Work time tracker api')
+    .setDescription('Work time tracker')
+    .addBearerAuth()
+    .build();
+
+  const options: SwaggerDocumentOptions = {
+    operationIdFactory: (controllerKey: string, methodKey: string) => methodKey,
+  };
+
+  const document = SwaggerModule.createDocument(app, config, options);
+  SwaggerModule.setup('/', app, document);
+}
+
 bootstrap();
