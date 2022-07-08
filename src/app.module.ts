e05a7dceb2d0cd246a709c2881f8ee9b6e11d504
Oguzhan Ozkaya <oguzdt@gmail.com>
2022-07-08 17:15:08 +0300

    mikro-orm sqlite

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,17 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    MikroOrmModule.forRoot({
+      entities: ['dist/**/*.entity.js'],
+      entitiesTs: ['src/**/*.entity.ts'],
+      dbName: 'work-time-tracker',
+      type: 'sqlite',
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
