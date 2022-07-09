046a00c3932e490850568be587faaec9da9cb2da
Oguzhan Ozkaya <oguzdt@gmail.com>
2022-07-09 12:51:44 +0300

    add baseDto

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,8 +1,9 @@
 import { ApiProperty } from '@nestjs/swagger';
 import { IsEmail, IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
+import { BaseDto } from '../../utils/base.dto';
 import { User } from '../user.entity';
 
-export class CreateUserDto implements Partial<User> {
+export class CreateUserDto implements BaseDto<User> {
   @IsNotEmpty()
   @MaxLength(50)
   @ApiProperty({ example: 'test' })
---
+++ src/utils/base.dto.ts
@@ -0,0 +1,1 @@
+export type BaseDto<T> = Partial<T>;
